4cc85d532674336b5004b3b2600a4687efbfd295
gabriel <gabrielsillva356@gmail.com>
2024-02-12 22:10:49 -0300

    esse arquivo foi uma das minhas primeiras vezes aprendendo o angular

---
+++ component/src/app/meu-primeiro/primeiro-component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'primeiro-component',
+  template: ` <p>Meu primeiro component com angular js</p> `,
+})
+export class PrimeiroComponent {}
